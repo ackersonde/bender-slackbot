d241b1607da642eb027b2177558f7dd07efdff15
Dan Ackerson <dan@ackerson.de>
2016-11-27 10:54:30 +0100

    too many args

--- commands/transmission.go
+++ commands/transmission.go
@@ -103,9 +103,9 @@ func torrentCommand(cmd []string) (result string) {
 		if cmd[0] == "trans" {
 			result = getTorrents(t)
 		} else if cmd[0] == "tranc" {
-			result = addTorrents(t, cmd[1], true)
+			result = addTorrents(t, cmd[1])
 		} else if cmd[0] == "trand" {
-			result = deleteTorrents(t, cmd[1], true)
+			result = deleteTorrents(t, cmd[1])
 		}
 
 		tunnelIdleSince = time.Now()
