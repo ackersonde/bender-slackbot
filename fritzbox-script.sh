38d9e61a8450f53f5e646b35f25ba6e3fc95019c
Dan Ackerson <dan@ackerson.de>
2017-02-25 12:25:59 +0100

    wait for tun0 to come up

--- fritzbox-script.sh
+++ fritzbox-script.sh
@@ -12,7 +12,12 @@ case "$reason" in
     $IPROUTE addr add "$INTERNAL_IP4_ADDRESS/255.255.255.0" peer "$INTERNAL_IP4_ADDRESS" dev "$TUNDEV"
     $IPROUTE route replace "$INTERNAL_IP4_PREFIX.0/255.255.255.0" dev "$TUNDEV"
     $IPROUTE route flush cache
-    route add -net 192.168.1.0/24 gw 192.168.178.1 dev tun0
+
+    while ! ip link show $TUNDEV >/dev/null 2>&1 ; do
+	     sleep 0.1
+    done
+
+    $IPROUTE route add 192.168.1.0/24 via 192.168.178.1
     ;;
   disconnect)
     $IPROUTE link set dev "$TUNDEV" down
