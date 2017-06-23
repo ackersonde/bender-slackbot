ea737975fd7db5319a6210d00476bb72dd826cbf
Dan Ackerson <dan@ackerson.de>
2017-06-23 11:50:22 +0200

    mvv links

--- commands/commands.go
+++ commands/commands.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/nlopes/slack"
 )
@@ -144,6 +145,17 @@ func CheckCommand(api *slack.Client, slackMessage slack.Msg, command string) {
 	} else if args[0] == "mvv" {
 		response := "<https://img.srv2.de/customer/sbahnMuenchen/newsticker/newsticker.html|Aktuelles>"
 		params := slack.PostMessageParameters{AsUser: true}
+
+		if len(args) > 1 {
+			if args[1] == "m" {
+				// show next train to MUC
+				response = "<" + mvvRoute("Schwabhausen", "München, Hauptbahnhof") + "|going to MUC>"
+			} else if args[1] == "s" {
+				// show next train to SCHWAB
+				response = "<" + mvvRoute("München, Hauptbahnhof", "Schwabhausen") + "|going home>"
+			}
+		}
+
 		api.PostMessage(slackMessage.Channel, response, params)
 	} else if args[0] == "help" {
 		response := ":sun_behind_rain_cloud: `sw`: Schwabhausen weather\n" +
@@ -162,6 +174,30 @@ func CheckCommand(api *slack.Client, slackMessage slack.Msg, command string) {
 		rtm.SendMessage(rtm.NewOutgoingMessage("whaddya say <@"+callingUserProfile.Name+">? Try `help` instead...",
 			slackMessage.Channel))
 	}
+}
+
+func mvvRoute(origin string, destination string) string {
+	loc, _ := time.LoadLocation("Europe/Berlin")
+	date := time.Now().In(loc)
+
+	yearObj := date.Year()
+	monthObj := int(date.Month())
+	dayObj := date.Day()
+	hourObj := date.Hour()
+	minuteObj := date.Minute()
+
+	month := strconv.Itoa(monthObj)
+	hour := strconv.Itoa(hourObj)
+	day := strconv.Itoa(dayObj)
+	minute := strconv.Itoa(minuteObj)
+	year := strconv.Itoa(yearObj)
+
+	return "http://efa.mvv-muenchen.de/mvv/XSLT_TRIP_REQUEST2?&language=de" +
+		"&anyObjFilter_origin=0&sessionID=0&itdTripDateTimeDepArr=dep&type_destination=any" +
+		"&itdDateMonth=" + month + "&itdTimeHour=" + hour + "&anySigWhenPerfectNoOtherMatches=1" +
+		"&locationServerActive=1&name_origin=" + origin + "&itdDateDay=" + day + "&type_origin=any" +
+		"&name_destination=" + destination + "&itdTimeMinute=" + minute + "&Session=0&stateless=1" +
+		"&SpEncId=0&itdDateYear=" + year
 }
 
 /* DownloadFile is now exported
