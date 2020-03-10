c6293e179130ad2fb2fc9c084fa1151b707a717b
Dan Ackerson <dan@ackerson.de>
2020-03-10 18:57:10 +0100

    add traefik links

--- commands/commands.go
+++ commands/commands.go
@@ -174,11 +174,13 @@ func CheckCommand(api *slack.Client, slackMessage slack.Msg, command string) {
 		api.PostMessage(slackMessage.Channel, slack.MsgOptionText(response, false), params)
 	} else if args[0] == "www" {
 		fritzBox := ":fritzbox: <https://fritz.ackerson.de/|fritz.box> | "
+		fritzBox += ":traefik: <https://monitor.ackerson.de/dashboard/#/ | traefik> | "
 		pi4 := ":k8s: <https://dash.ackerson.de/#/overview?namespace=default|k8s> | "
 		pi4 += ":netdata: <http://pi4:19999/#menu_cpu_submenu_utilization;theme=slate;help=true|netdata>\n"
-		pi4 += ":pihole: <https://hole.ackerson.de/admin/| pi.hole > | "
+		pi4 += ":pihole: <https://hole.ackerson.de/admin/|pi.hole> | "
 		vpnpi := ":transmission: <http://vpnpi:9091/transmission/web/|trans> | "
-		vpnpi += ":plex: <http://vpnpi:32400/web/index.html#|plex>\n"
+		vpnpi += ":plex: <http://vpnpi:32400/web/index.html#|plex> | "
+		vpnpi += ":traefik: <https://api-wc-gcp.ackerson.de/dashboard/#/ | weechat>\n"
 
 		response := fritzBox + pi4 + vpnpi
 		api.PostMessage(slackMessage.Channel, slack.MsgOptionText(response, false), params)
