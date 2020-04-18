37760c35125390b8a22724f53899edf32a08777f
Dan Ackerson <dan@ackerson.de>
2020-04-18 09:49:09 +0200

    use new pb api

--- commands/searches.go
+++ commands/searches.go
@@ -3,16 +3,17 @@ package commands
 // forked from https://github.com/jasonrhansen/piratebay
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strconv"
-	"strings"
 	"time"
 
-	"golang.org/x/net/html"
+	"github.com/danackerson/bender-slackbot/structures"
 )
 
 // Category is the type of torrent to search for.
@@ -33,7 +34,7 @@ const (
 
 var proxies = []string{"tpb.cool", "piratebay.tech", "thepiratebay.fail", "piratebay.icu", "thepirate.host"}
 
-func searchProxy(url string) []Torrent {
+func searchProxy(url string) []structures.Torrent {
 	for i, proxy := range proxies {
 		uri := "https://" + proxy + url
 		log.Printf("torq try #%d: %s ...\n", i, uri)
@@ -53,11 +54,14 @@ func searchProxy(url string) []Torrent {
 			continue
 		}
 		if resp != nil {
-			doc, err := html.Parse(resp.Body)
-			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Printf("failed to parse JSON: %s", err.Error())
+				continue
+			}
 
-			if err == nil && doc != nil {
-				torrents := getTorrentsFromDoc(doc)
+			if err == nil && body != nil {
+				torrents := getTorrentsFromJSON([]byte(body))
 				if torrents != nil {
 					log.Printf("%s%s succeeded!", proxy, url)
 					return torrents
@@ -69,40 +73,28 @@ func searchProxy(url string) []Torrent {
 	return nil
 }
 
-// Torrent stores information about a torrent found on The Pirate Bay.
-type Torrent struct {
-	Title       string
-	MagnetLink  string
-	Size        int
-	Uploaded    string
-	User        string
-	VIP         bool
-	Trusted     bool
-	Seeders     int
-	Leechers    int
-	Category    string
-	CategoryID  int
-	DetailsLink string
-}
-
 // SearchFor is now commented
-func SearchFor(term string, cat Category) ([]Torrent, string) {
+func SearchFor(term string, cat Category) ([]structures.Torrent, string) {
 	response := ""
 
-	var torrents []Torrent
+	var torrents []structures.Torrent
 	torrents, err := search(term, cat)
 	found := 0
 	if err == nil {
 		for i, t := range torrents {
-			if t.Seeders > 10 {
+			seeders, _ := strconv.Atoi(t.Seeders)
+			if seeders > 10 {
 				found++
-				humanSize := float64(t.Size / (1024 * 1024))
+				size, _ := strconv.Atoi(t.Size)
+				humanSize := float64(size / (1024 * 1024))
 				sizeSuffix := fmt.Sprintf("*%.0f MiB*", humanSize)
 				if humanSize > 999 {
 					humanSize = humanSize / 1024
 					sizeSuffix = fmt.Sprintf("*%.1f GiB*", humanSize)
 				}
-				response += fmt.Sprintf("%d: <http://%s|%s> Seeds:%d %s\n", i, t.MagnetLink, t.Title, t.Seeders, sizeSuffix)
+
+				magnetLink := fmt.Sprintf("magnet/?xt=urn:btih:%s", t.InfoHash)
+				response += fmt.Sprintf("%d: <http://%s|%s> Seeds:%d %s\n", i, magnetLink, t.Name, seeders, sizeSuffix)
 			}
 		}
 	} else {
@@ -117,8 +109,8 @@ func SearchFor(term string, cat Category) ([]Torrent, string) {
 }
 
 // search returns the torrents found with the given search string and categories.
-func search(query string, cats ...Category) ([]Torrent, error) {
-	var torrents []Torrent
+func search(query string, cats ...Category) ([]structures.Torrent, error) {
+	var torrents []structures.Torrent
 
 	if query != "" {
 		if len(cats) == 0 {
@@ -136,10 +128,10 @@ func search(query string, cats ...Category) ([]Torrent, error) {
 			catStr = "0"
 		}
 
-		searchStringURL := "/search.php?q=" + url.QueryEscape(query) + "&cat=" + catStr
+		searchStringURL := "/api?url=/q.php?q=" + url.QueryEscape(query) + "&cat=" + catStr
 		torrents = searchProxy(searchStringURL)
 	} else {
-		torrents = searchProxy("/search.php?q=category:207")
+		torrents = searchProxy("/api?url=/q.php?q=category%3A207")
 	}
 
 	if torrents == nil {
@@ -149,111 +141,14 @@ func search(query string, cats ...Category) ([]Torrent, error) {
 	return torrents, nil
 }
 
-func getTorrentsFromDoc(doc *html.Node) []Torrent {
-	tc := make(chan Torrent)
-	go func() {
-		loopDOM(doc, tc)
-		close(tc)
-	}()
-	var torrents []Torrent
-	for t := range tc {
-		torrents = append(torrents, t)
-	}
+func getTorrentsFromJSON(jsonObject []byte) []structures.Torrent {
+	var torrentResp structures.TorrentAPIResponse
 
-	return torrents
-}
-
-func loopDOM(n *html.Node, tc chan Torrent) {
-	if n.Type == html.ElementNode && n.Data == "tbody" {
-		getTorrentsFromTable(n, tc)
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		loopDOM(c, tc)
-	}
-}
-
-func getTorrentsFromTable(n *html.Node, tc chan Torrent) {
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		if c.Type == html.ElementNode && c.Data == "tr" {
-			getTorrentFromRow(c, tc)
-		}
-	}
-}
-
-func getTorrentFromRow(n *html.Node, tc chan Torrent) {
-	var torrent Torrent
-	col := 0
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		if c.Type == html.ElementNode && c.Data == "td" {
-			setTorrentDataFromCell(c, &torrent, col)
-			col++
-		}
-	}
-	tc <- torrent
-}
-
-func setTorrentDataFromCell(n *html.Node, t *Torrent, col int) {
-	if n.Type == html.ElementNode {
-		if col == 2 {
-			if s, err := strconv.Atoi(getNodeText(n)); err == nil {
-				t.Seeders = s
-			}
-		} else if col == 3 {
-			if l, err := strconv.Atoi(getNodeText(n)); err == nil {
-				t.Leechers = l
-			}
-		} else {
-			for _, a := range n.Attr {
-				if n.Data == "a" && a.Key == "href" {
-					if strings.HasPrefix(a.Val, "magnet") {
-						t.MagnetLink = a.Val
-					} else if strings.Contains(a.Val, "/torrent/") {
-						if t.Title == "" {
-							t.Title = getNodeText(n)
-							t.DetailsLink = a.Val
-						}
-					}
-				} else if n.Data == "font" && a.Key == "class" && a.Val == "detDesc" {
-					parts := strings.Split(getNodeText(n), ", ")
-					if len(parts) > 1 {
-						t.Uploaded = strings.Split(parts[0], " ")[1]
-						t.Size = sizeStrToInt(strings.Split(parts[1], " ")[1])
-					}
-				}
-			}
-		}
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		setTorrentDataFromCell(c, t, col)
-	}
-}
-
-func sizeStrToInt(s string) int {
-	var multiply int
-	if len(s) < 5 {
-		return 0
-	}
-	multiply = 1
-	ext := s[len(s)-3:]
-	if ext == "MiB" {
-		multiply = 1024 * 1024
-	} else if ext == "KiB" {
-		multiply = 1024
-	} else if ext == "GiB" {
-		multiply = 1024 * 1024 * 1024
-	}
-	size, err := strconv.ParseFloat(s[:len(s)-5], 64)
+	var s = new(structures.TorrentAPIResponse)
+	err := json.Unmarshal(jsonObject, &s)
 	if err != nil {
-		return 0
+		fmt.Println("whoops:", err)
 	}
-	return int(size * float64(multiply))
-}
 
-func getNodeText(n *html.Node) string {
-	for a := n.FirstChild; a != nil; a = a.NextSibling {
-		if a.Type == html.TextNode {
-			return strings.TrimSpace(a.Data)
-		}
-	}
-	return ""
+	return torrentResp.TorrentList
 }
---
+++ structures/torrentFilesJSON.go
@@ -0,0 +1,22 @@
+package structures
+
+// TorrentAPIResponse object from pb
+type TorrentAPIResponse struct {
+	TorrentList []Torrent
+}
+
+// Torrent response object from pb
+type Torrent struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	InfoHash string `json:"info_hash"`
+	Leechers string `json:"leechers"`
+	Seeders  string `json:"seeders"`
+	NumFiles string `json:"num_files"`
+	Size     string `json:"size"`
+	Username string `json:"username"`
+	Added    string `json:"added"`
+	Status   string `json:"status"`
+	Category string `json:"category"`
+	Imdb     string `json:"imdb"`
+}
