25646a440dd2d4b92dcfeb5878a419e84c834cdf
Dan Ackerson <dan@ackerson.de>
2020-05-17 21:55:24 +0200

    add anon to init struct

--- structures/torrentFilesJSON.go
+++ structures/torrentFilesJSON.go
@@ -1,6 +1,6 @@
 package structures
 
-// Torrent response object from pb
+// Torrents response object from pb
 type Torrents []struct {
 	ID       string `json:"id"`
 	Name     string `json:"name"`
@@ -14,4 +14,22 @@ type Torrents []struct {
 	Status   string `json:"status"`
 	Category string `json:"category"`
 	Imdb     string `json:"imdb"`
+	Anon     int    `json:"anon,omitempty"`
+}
+
+// Top100Movies listing has an Anon field
+type Top100Movies []struct {
+	ID       int         `json:"id"`
+	InfoHash string      `json:"info_hash"`
+	Category int         `json:"category"`
+	Name     string      `json:"name"`
+	Status   string      `json:"status"`
+	NumFiles int         `json:"num_files"`
+	Size     int         `json:"size"`
+	Seeders  int         `json:"seeders"`
+	Leechers int         `json:"leechers"`
+	Username string      `json:"username"`
+	Added    int         `json:"added"`
+	Anon     int         `json:"anon,omitempty"`
+	Imdb     interface{} `json:"imdb"` // e.g. https://www.imdb.com/title/<Imdb-string>
 }
