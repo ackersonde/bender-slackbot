e45fcd4fcd1313ad3b66fc183b0c74687666585a
danackerson <dan@ackerson.de>
2020-10-17 11:39:10 +0200

    let slack know we rcvd the event

--- bender.go
+++ bender.go
@@ -89,10 +89,6 @@ func parseSlackEvent(w http.ResponseWriter, r *http.Request) slackevents.EventsA
 func processMessage(api *slack.Client, ev *slackevents.MessageEvent) {
 	originalMessage := ev.Text
 
-	// TODO: Huge! determine if the requested Event is already being processed
-	// if so, ignore it :( you'll need some global event.process channel
-	// upon completion, this event.process channel should be flushed
-
 	if ev.User != "" && ev.User != botID && ev.User != "U2NQSPHHD" &&
 		ev.SubType != "message_deleted" &&
 		(strings.Contains(ev.Text, "<@"+botID+">") ||
@@ -103,8 +99,7 @@ func processMessage(api *slack.Client, ev *slackevents.MessageEvent) {
 		r, n := utf8.DecodeRuneInString(parsedMessage)
 		parsedMessage = string(unicode.ToLower(r)) + parsedMessage[n:]
 
-		commands.Logger.Printf("%s(%s) asks '%v'\nEventTimestamp: %s \t ThreadTimestamp: %s \t Timestamp: %s\n",
-			ev.Username, ev.User, parsedMessage, ev.EventTimeStamp, ev.ThreadTimeStamp, ev.TimeStamp)
+		commands.Logger.Printf("%s(%s) asks '%v'\n", ev.Username, ev.User, parsedMessage)
 		commands.CheckCommand(api, ev, parsedMessage)
 	}
 }
@@ -124,6 +119,8 @@ func main() {
 				innerEvent := eventsAPIEvent.InnerEvent
 				switch ev := innerEvent.Data.(type) {
 				case *slackevents.MessageEvent:
+					// HTTP 202 -> we heard and are working on an async response
+					w.WriteHeader(http.StatusAccepted)
 					processMessage(api, ev)
 				}
 			}
