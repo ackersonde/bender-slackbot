d48a6df3dad70d7f3847a54c5c165e878dc6f51a
danackerson <dan@ackerson.de>
2022-07-07 13:47:25 +0200

    show the mangled dates

--- commands/baseball.go
+++ commands/baseball.go
@@ -38,9 +38,8 @@ func ShowBBGames(fromDate string) string {
 
 	for _, gameMetaData := range response.Games {
 		watchURL := "<" + gameMetaData[10] + "|" + gameMetaData[0] + " @ " + gameMetaData[4] + ">"
-		Logger.Printf("WTF: %s", response.ReadableDate)
-		downloadURL := "<https://ackerson.de/bb_download?gameTitle=" + gameMetaData[2] + "-" + gameMetaData[6] + "__" + response.ReadableDate + "&gameURL=" + gameMetaData[10] + "|:red_dot: >"
-
+		downloadURL := "<https://ackerson.de/bb_download?gameTitle=" + gameMetaData[2] + "-" + gameMetaData[6] + "__" + response.ReadableDate + "&gameURL=" + gameMetaData[10] + "|:red_dot:> "
+		Logger.Printf("WTF: %s", downloadURL)
 		result += downloadURL + watchURL + "\n"
 	}
 
