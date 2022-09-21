0383b2bff604b1077bc3251ac6124160859d2ad5
Dan Ackerson <dan@ackerson.de>
2022-09-21 14:48:13 +0200

    escape

--- commands/commands.go
+++ commands/commands.go
@@ -52,12 +52,12 @@ func CheckCommand(event *slackevents.MessageEvent, user *slack.User, command str
 
 	if args[0] == "h4X07s" {
 		response := ""
-		sshConfig := structures.PI4RemoteConnectConfig
 		cmdPrefix := "ssh vault "
-		ipv6Prefix := "2a02:810d:c0:581b" // TODO: replace w/ fetchHomeIPv6Prefix
-		cmd := fmt.Sprintf("%sawk '{print $1}' traefik/logs/access.log | sort -n | uniq -c | sort -nr | head -20 | grep -v %s", cmdPrefix, ipv6Prefix)
+		ipv6Prefix := strings.Split(fetchHomeIPv6Prefix(), "/")
 
-		remoteResult := executeRemoteCmd(cmd, sshConfig)
+		cmd := fmt.Sprintf("%sawk '{print \\$1}' traefik/logs/access.log | grep -v %s | sort -n | uniq -c | sort -nr | head -10", cmdPrefix, ipv6Prefix[0])
+
+		remoteResult := executeRemoteCmd(cmd, structures.PI4RemoteConnectConfig)
 
 		if remoteResult.Stdout == "" && remoteResult.Stderr != "" {
 			response = remoteResult.Stderr
